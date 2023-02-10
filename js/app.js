b2b04f5f278dbcd9eacf3b02fd1598d482cfed4f
Lapo0 <lapo.gini.01@gmail.com>
2023-02-10 15:53:38 +0100

    FEAT oneclick for cella

--- js/app.js
+++ js/app.js
@@ -1,18 +1,17 @@
-console.log('prova')
-
+// console.log('prova')
 // Creare bottone per richiamare click
 const play = document.getElementById('btn-play')
-console.log(play)
+// console.log(play)
 
 const griglia = document.querySelector('.griglia')
-console.log(griglia)
+// console.log(griglia)
 
 play.addEventListener('click', function() {
 
     // Creare un input dove l'utente possa scegliere il livello
     // Selezionare input dall'HTML
     const levelUser = document.getElementById('level')
-    console.log(levelUser)
+    // console.log(levelUser)
 
     // Prendere il valore dell'utente
     const levelPc = levelUser.value
@@ -23,12 +22,11 @@ play.addEventListener('click', function() {
     console.log(levelPc)
 
     const level = levelPc * 2
-    
 
     // Celle sul lato e celle totali
     let latoGrid = level
     let totalGrid = latoGrid ** 2
-    console.log(latoGrid, totalGrid)
+    // console.log(latoGrid, totalGrid)
 
     //Azzerare HTML griglia
     griglia.innerHTML = ''
@@ -40,7 +38,7 @@ play.addEventListener('click', function() {
 
         // Creare i div con il numero corrispondente all'interno e la grandezza in base ai lati
         const cellString = `<div class='ratio ratio-1x1 cella border-secondary' style='width: calc(100% / ${ latoGrid })'> ${ num } </div>`
-        console.log(cellString)
+        // console.log(cellString)
 
         //Aggiungere all'html il div
         griglia.innerHTML += cellString
@@ -48,8 +46,7 @@ play.addEventListener('click', function() {
 
     // Creare l'elemento div
     const celleElements = document.querySelectorAll('.cella')
-    console.log(celleElements)
-
+    // console.log(celleElements)
     
     // Creare un ciclo per selezionare tutte le celle
     for (let i = 0; i < celleElements.length; i++) {
@@ -60,17 +57,12 @@ play.addEventListener('click', function() {
 
         // Creare un evento click dove stampare ciao
         cella.addEventListener('click', function() {
-        console.log(num)
-
-            // Aggiungere background
-            cella.classList.add('bg-secondary')
-
-
+            if (!cella.classList.contains('bg-secondary')) {
+                console.log(num)
+    
+                // Aggiungere background
+                cella.classList.add('bg-secondary')
+            }
         })
     }
-    
-    
-    
-
-
 })
